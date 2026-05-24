660c8c1d8385c443636bc2f852bc058860f3b684
Nguyen Quang Huy <quanghuy190105@gmail.com>
2026-05-24 18:33:04 +0700

    update logic for notepad and todo-list

--- my-app/src/features/notes/ui/Notepad.jsx
+++ my-app/src/features/notes/ui/Notepad.jsx
@@ -1,7 +1,103 @@
-export default function NotePad() {
+import { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import { Form } from 'react-router-dom'
+import '../css/Styles.css'
+
+function ModalInput({ show, onHide, note, onTitleChange, onContentInput, status }) {
     return (
-        <div>
-            <h2 className="text-primary">Your Notepad will show here</h2>
-        </div>
+        <Modal
+            show={show}
+            onHide={onHide}
+            size="lg"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>
+                    <h4>{note.title || 'Untitled note'}</h4>
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <input type="text"
+                        placeholder='Title...'
+                        className='mb-3 form-control'
+                        value={note.title}
+                        onChange={onTitleChange}
+                        disabled={status === 'submiting'}
+                    />
+                </Form>
+                <div className='form-control note-editor'
+                    contentEditable
+                    data-placeholder='Content...'
+                    onInput={onContentInput}
+                >
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Form action=''>
+                    <input className='d-none' type='text' value={note.title == '' ? 'Untitled note' : note.title} readOnly />
+                    <textarea className='d-none' value={note.contentText} readOnly></textarea>
+                    <Button className='me-3'
+                        type='submit'
+                        disabled={
+                            note.contentText.length === 0 || status === 'submitting'
+                        }
+                    >Save</Button>
+                    <Button variant='secondary' onClick={onHide}>Close</Button>
+                </Form>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+export default function NotePad() {
+    const [note, setNote] = useState({
+        title: '',
+        contentText: '',
+        contentHtml: '',
+    });
+    const [modalShow, setModalShow] = useState(false);
+    const [status, setStatus] = useState('typing');
+    const [error, setError] = useState(null);
+    if (status === 'success') {
+        return alert('Save successfully !');
+    }
+    return (
+        <>
+            <div className='d-flex justify-content-end'>
+                <Button variant="primary" onClick={() => setModalShow(true)}>
+                    Create a notepad
+                </Button>
+            </div>
+            <ModalInput
+                status={status}
+                show={modalShow}
+                onHide={() => {
+                    setModalShow(false);
+                    setNote({
+                        title: '',
+                        contentText: '',
+                        contentHtml: '',
+                    })
+                }}
+                note={note}
+                onTitleChange={(e) => {
+                    setNote((note) => ({ ...note, title: e.target.value }));
+                }}
+                onContentInput={(e) => {
+                    const el = e.currentTarget;
+                    if (!el.textContent?.trim()) {
+                        el.innerHTML = '';
+                    }
+                    setNote((note) => ({
+                        ...note,
+                        contentText: el.innerText,
+                        contentHtml: el.innerHTML,
+                    }));
+                }}
+            />
+            <p>{note.contentText}</p>
+        </>
+
+
     );
 }
--- my-app/src/features/notes/ui/Todolist.jsx
+++ my-app/src/features/notes/ui/Todolist.jsx
@@ -1,7 +1,138 @@
-export default function NoteTodolist() {
+import { useState } from 'react';
+import { Modal, Button, Row, Col, Form } from 'react-bootstrap';
+import '../css/Styles.css'
+
+function Todo({ obj, todos, setTodos }) {
+    let newChecked;
+    if (obj.checked) {
+        newChecked = false;
+    }
+    else newChecked = true;
+    const onChangeChecked = () => {
+        setTodos(() =>
+            todos.map(todo => (todo.id === obj.id ? { ...todo, checked: newChecked } : todo)
+            )
+        )
+    };
     return (
-        <div>
-            <h2 className="text-primary">Your todolist will show here</h2>
-        </div>
+        <>
+            <Row>
+                <Col xs={12} md={8}>
+                    <input type="checkbox" checked={obj.checked} onChange={onChangeChecked} />
+                    <span className='ms-2'>{!obj.checked ? obj.content : <del>{obj.content}</del>}</span>
+                </Col>
+                <Col xs={12} md={4}>
+                    <Button className='me-3 ms-5' size='sm'>Edit</Button>
+                    <Button size='sm'>Delete</Button>
+                </Col>
+            </Row>
+        </>
+    );
+}
+
+function ModalInput({ show, onHide, todo, todos, onTitleChange, onContentInput, title, onAddToList, setTodos }) {
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            size="lg"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>
+                    <h4>{title || 'Untitled'}</h4>
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Row>
+                        <Col xs={12} md={12}>
+                            <input type="text"
+                                placeholder='Title...'
+                                className='mb-3 form-control'
+                                value={title}
+                                onChange={onTitleChange}
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} md={9}>
+                            <input type="text"
+                                placeholder='Add your task here '
+                                className='mb-3 form-control'
+                                value={todo.content}
+                                onChange={onContentInput}
+                            />
+                        </Col>
+                        <Col xs={12} md={3}>
+                            <Button type="button" onClick={onAddToList} >Create new task</Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} md={12}>
+                            <ul>
+                                {todos.map((obj) => (
+                                    <li key={obj.id} className='mb-3'>
+                                        <Todo obj={obj} setTodos={setTodos} todos={todos} />
+                                    </li>
+                                ))}
+                            </ul>
+                        </Col>
+                    </Row>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button className='me-3' type='submit'>Save</Button>
+                <Button variant='secondary' onClick={onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+export default function NoteTodolist() {
+    const [title, setTitle] = useState('');
+    const [todo, setTodo] = useState({
+        content: '',
+        checked: false
+    });
+    const [todos, setTodos] = useState([]);
+    const [modalShow, setModalShow] = useState(false);
+    const [error, setError] = useState(null);
+    return (
+        <>
+            <div className='d-flex justify-content-end'>
+                <Button variant="primary" onClick={() => setModalShow(true)}>
+                    Create a new Todo-list
+                </Button>
+            </div>
+            <ModalInput
+                show={modalShow}
+                title={title}
+                onHide={() => {
+                    setModalShow(false);
+                }}
+                onTitleChange={(e) => {
+                    setTitle(e.target.value);
+                }}
+                todo={todo}
+                todos={todos}
+                onContentInput={(e) => {
+                    setTodo(todo => ({
+                        ...todo,
+                        content: e.target.value,
+                    }))
+                }}
+                onAddToList={() => {
+                    if (!todo.content.trim()) return;
+                    setTodos([
+                        ...todos,
+                        { id: crypto.randomUUID(), ...todo }
+                    ]);
+                    setTodo({ content: '', checked: false });
+                }}
+                setTodos={setTodos}
+            />
+        </>
+
+
     );
 }
